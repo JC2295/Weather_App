8fcabd50c75f14ad0266e78d66555a02a77240a3
Jonathan Copeland <jonathan.copeland@hotmail.co.uk>
2024-02-26 20:15:57 +0000

    Started weather app.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Define Weather API URL
+const apiUrl = "https://api.open-meteo.com/v1/dwd-icon?latitude=51.4552&longitude=-2.5966&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure&hourly=temperature_2m";
 
+// Make a GET request
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
